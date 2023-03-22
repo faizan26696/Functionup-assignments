3a7fe6d7c88b56f337f860b347999445a177c8de
Mohammad Faizan <111674496+faizan26696@users.noreply.github.com>
2023-03-22 17:06:43 +0530

    mar-21-card-scroll-assign

---
+++ march-21-assignments/card-scroll-assign/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Dashboard from "./Components/DashBoard/Dashboard";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
