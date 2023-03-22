95ff1b91828e9513838096a4c8eb7802ce9acb96
Mohammad Faizan <111674496+faizan26696@users.noreply.github.com>
2023-03-22 17:10:32 +0530

    mar-21-scroll-card-assing

---
+++ march-21-assignments/card-scroll-assign/src/Components/Card.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import style from "./Card.module.css";
+
+const Card = () => {
+  return (
+    <div className={style.maincontainer}>
+      <div className={style.CardSection}>
+        <div className={style.card}>
+          <img
+            src="https://media.istockphoto.com/id/1198252595/photo/cheerful-indian-girl-standing-at-home-office-looking-at-camera.jpg?s=1024x1024&w=is&k=20&c=nqRJ4GR2qi6eRzwlNd9nMNeihlR-d64p4uHxlf2tu78="
+            alt=""
+          />
+          <h4>lesio Alexander</h4>
+          <span>Product Manager</span>
+          <h6 style={{ color: "blue" }}>4 years Exp</h6>
+        </div>
+
+        <div className={style.card}>
+          <img
+            src="https://media.istockphoto.com/id/1198252595/photo/cheerful-indian-girl-standing-at-home-office-looking-at-camera.jpg?s=1024x1024&w=is&k=20&c=nqRJ4GR2qi6eRzwlNd9nMNeihlR-d64p4uHxlf2tu78="
+            alt=""
+          />
+          <h4>lesio Alexander</h4>
+          <span>Product Manager</span>
+          <h6 style={{ color: "blue" }}>4 years Exp</h6>
+        </div>
+
+        <div className={style.card}>
+          <img
+            src="https://media.istockphoto.com/id/1198252595/photo/cheerful-indian-girl-standing-at-home-office-looking-at-camera.jpg?s=1024x1024&w=is&k=20&c=nqRJ4GR2qi6eRzwlNd9nMNeihlR-d64p4uHxlf2tu78="
+            alt=""
+          />
+          <h4>lesio Alexander</h4>
+          <span>Product Manager</span>
+          <h6 style={{ color: "blue" }}>4 years Exp</h6>
+        </div>
+
+        <div className={style.card}>
+          <img
+            src="https://media.istockphoto.com/id/1198252595/photo/cheerful-indian-girl-standing-at-home-office-looking-at-camera.jpg?s=1024x1024&w=is&k=20&c=nqRJ4GR2qi6eRzwlNd9nMNeihlR-d64p4uHxlf2tu78="
+            alt=""
+          />
+          <h4>lesio Alexander</h4>
+          <span>Product Manager</span>
+          <h6 style={{ color: "blue" }}>4 years Exp</h6>
+        </div>
+
+        <div className={style.card}>
+          <img
+            src="https://media.istockphoto.com/id/1198252595/photo/cheerful-indian-girl-standing-at-home-office-looking-at-camera.jpg?s=1024x1024&w=is&k=20&c=nqRJ4GR2qi6eRzwlNd9nMNeihlR-d64p4uHxlf2tu78="
+            alt=""
+          />
+          <h4>lesio Alexander</h4>
+          <span>Product Manager</span>
+          <h6 style={{ color: "blue" }}>4 years Exp</h6>
+        </div>
+
+        <div className={style.card}>
+          <img
+            src="https://media.istockphoto.com/id/1198252595/photo/cheerful-indian-girl-standing-at-home-office-looking-at-camera.jpg?s=1024x1024&w=is&k=20&c=nqRJ4GR2qi6eRzwlNd9nMNeihlR-d64p4uHxlf2tu78="
+            alt=""
+          />
+          <h4>lesio Alexander</h4>
+          <span>Product Manager</span>
+          <h6 style={{ color: "blue" }}>4 years Exp</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
